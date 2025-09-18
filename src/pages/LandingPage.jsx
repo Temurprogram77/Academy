fa5d8947617fd49d6aa9ee6a945ec3eb51f7c5e1
Temur Programmer <temurnarzullayev810@gmail.com>
2025-09-18 15:23:34 +0500

    added

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,14 +5,14 @@ const { logo, main } = dataImages;
 
 const LandingPage = () => {
   return (
-    <div className="md:mx-auto h-screen mx-4 flex flex-col items-center justify-center">
-      <div className="logo mb-5">
-        <img src={logo} alt="logo" className="w-[200px]" />
+    <div className="md:mx-auto w-[400px] h-screen mx-4 flex flex-col items-center justify-center">
+      <div className="logo mb-5 w-full">
+        <img src={logo} alt="logo" className="w-[100px]" />
       </div>
       <h2 className="text-[40px] max-w-[400px] font-semibold text-center leading-9">
         Find out your English level!
       </h2>
-      <img className="md:w-[500px]" src={main} alt="main" />
+      <img className="w-full" src={main} alt="main" />
       <Link to="/login" className="sm:max-w-[420px] w-full">
         <button className="cursor-pointer btn-gradient w-full py-2 text-white font-semibold rounded-3xl">
           Start
